a2e0eebe7a9a085391e1f145544036423c3130aa
Fabio Garcia <fabio.garcia@moove-it.com>
2020-10-30 08:46:34 -0500

    Revolves exercises 5 to 8, exercise 9 still in process

--- exercises/exercise-05/index.ts
+++ exercises/exercise-05/index.ts
@@ -34,49 +34,57 @@ Run:
 */
 
 interface User {
-    type: 'user';
-    name: string;
-    age: number;
-    occupation: string;
+  type: 'user';
+  name: string;
+  age: number;
+  occupation: string;
 }
 
 interface Admin {
-    type: 'admin';
-    name: string;
-    age: number;
-    role: string;
+  type: 'admin';
+  name: string;
+  age: number;
+  role: string;
 }
 
 type Person = User | Admin;
 
 const persons: Person[] = [
-    { type: 'user', name: 'Max Mustermann', age: 25, occupation: 'Chimney sweep' },
-    { type: 'admin', name: 'Jane Doe', age: 32, role: 'Administrator' },
-    { type: 'user', name: 'Kate Müller', age: 23, occupation: 'Astronaut' },
-    { type: 'admin', name: 'Bruce Willis', age: 64, role: 'World saver' },
-    { type: 'user', name: 'Wilson', age: 23, occupation: 'Ball' },
-    { type: 'admin', name: 'Agent Smith', age: 23, role: 'Anti-virus engineer' }
+  {type: 'user', name: 'Max Mustermann', age: 25, occupation: 'Chimney sweep'},
+  {type: 'admin', name: 'Jane Doe', age: 32, role: 'Administrator'},
+  {type: 'user', name: 'Kate Müller', age: 23, occupation: 'Astronaut'},
+  {type: 'admin', name: 'Bruce Willis', age: 64, role: 'World saver'},
+  {type: 'user', name: 'Wilson', age: 23, occupation: 'Ball'},
+  {type: 'admin', name: 'Agent Smith', age: 23, role: 'Anti-virus engineer'}
 ];
 
 function logPerson(person: Person) {
-    console.log(
-        ` - ${chalk.green(person.name)}, ${person.age}, ${person.type === 'admin' ? person.role : person.occupation}`
-    );
+  console.log(
+    ` - ${chalk.green(person.name)}, ${person.age}, ${person.type === 'admin' ? person.role : person.occupation}`
+  );
 }
 
-function filterPersons(persons: Person[], personType: string, criteria: unknown): unknown[] {
-    return persons
-        .filter((person) => person.type === personType)
-        .filter((person) => {
-            let criteriaKeys = Object.keys(criteria) as (keyof Person)[];
-            return criteriaKeys.every((fieldName) => {
-                return person[fieldName] === criteria[fieldName];
-            });
-        });
+function getObjectKeys<T>(criteria: CriteriaOptional<T>) {
+  return Object.keys(criteria) as (keyof Person)[]
 }
 
-let usersOfAge23: User[] = filterPersons(persons, 'user', { age: 23 });
-let adminsOfAge23: Admin[] = filterPersons(persons, 'admin', { age: 23 });
+type CriteriaOptional<T> = {
+  [P in keyof T]?: T[P];
+};
+
+function filterPersons<T extends Person>(persons: Person[], personType: string, criteria: CriteriaOptional<T>): Person[] {
+  return persons
+    .filter((person) => person.type === personType)
+    .filter((person) => {
+      let criteriaKeys = getObjectKeys<T>(criteria);
+      return criteriaKeys.every((fieldName) => {
+        return person[fieldName] === criteria[fieldName];
+      });
+    });
+}
+
+let usersOfAge23: Person[] = filterPersons<User>(persons, 'user', {age: 23});
+let adminsOfAge23: Person[] = filterPersons<Admin>(persons, 'admin', {age: 23});
 
 console.log(chalk.yellow('Users of age 23:'));
 usersOfAge23.forEach(logPerson);
